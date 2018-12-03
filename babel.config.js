57c8c68a4fdfba3bcea3ee469e450c7bee0bffc2
Zhang Wen <zhangwen1932@hotmail.com>
2018-12-04 00:37:08 +0800

    增加插件

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   const presets = ["@babel/preset-react"];
-  // const plugins = [... ];
+  const plugins = ['@babel/plugin-proposal-class-properties'];
 
   return {
     presets,
-    // plugins
+    plugins
   };
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import test from './img/test.jpg';
 
 class App extends Component{
+  state = {
+    reset:'yes'
+  }
+  onClick =() => {
+    this.setState({
+      reset:this.state.reset==='yes'?'no':'yes'
+    })
+  }
   render(){
     return(
       <div>
-        <img src={test} alt='flower'/>
+        <img 
+        src={test} 
+        alt='flower' 
+        className={this.state.reset === 'yes'? 'flower' : 'flower flower-rotate'}
+        onClick={this.onClick}
+        />
       </div>
     )
   }
