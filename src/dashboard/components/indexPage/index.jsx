6a353f7f798b355ad79509ca68679434f73adde6
Zhang Wen <zhangwen1932@hotmail.com>
2018-12-11 17:58:27 +0800

    小修改

--- src/dashboard/components/indexPage/index.jsx
+++ src/dashboard/components/indexPage/index.jsx
@@ -7,12 +7,9 @@ import './style.scss';
 function IndexPage() {
   return (
     <div className="normal">
-      <h1 className="title">测试热加载是否成功</h1>
+      <h1 className="title">Hello world!</h1>
       <div className="welcome" />
       <ul className="list">
-        <li>
-          <h1>Hello world</h1>
-        </li>
         <li>
           <Button type="primary">Getting Started</Button>
         </li>
